304759aa96e46937cb1d2e1365d6d430d2063483
Iryna Holovata <irina-golovataja@ukr.net>
2020-08-28 21:49:42 +0300

    first commit

---
+++ src/main/java/com/smida/test/task/smida/SmidaShareRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.smida.test.task.smida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmidaShareRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmidaShareRegisterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smida/test/task/smida/SmidaShareRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smida.test.task.smida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmidaShareRegisterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
